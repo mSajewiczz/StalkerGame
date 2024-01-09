8e08b59674c5e966b1e7e07600dc6ff5111f3359
Mikołaj Sajewicz <107215730+mSajewiczz@users.noreply.github.com>
2024-01-09 17:33:21 +0100

    Last JS version

--- main.js
+++ main.js
@@ -1,24 +1,128 @@
-const hide = document.querySelector('.hide')
-const active = document.querySelector('.active')
+const taskTxt = document.querySelector(".tast-txt")
 
-const on = document.querySelector('.btn-on')
-const off = document.querySelector('.btn-off')
+let numOne = 0;
 
-const task = document.querySelector('.task')
-const one = document.querySelector('.task-one')
-const two = document.querySelector('.task-one')
-const three = document.querySelector('.task-one')
-const four = document.querySelector('.task-one')
-const five = document.querySelector('.task-one')
-
-
-function showNext () {
-    one.classList.add('active')
+function add() {
+    if(numOne < 12) {
+        return ++numOne;
+    }
 }
 
-function hideFunc () {
-    one.classList.remove('active')
+function sub() {
+    if(numOne > 0) {
+        return --numOne;
+    }
 }
 
-  on.addEventListener('click', showNext)
-    off.addEventListener('click', hideFunc)
+function sw1() {
+    add()
+    switch (numOne) {
+        case 0:
+            taskTxt.innerHTML = "Kliknij przycisk, aby pokazać treść...";
+            break;
+
+        case 1:
+            taskTxt.innerHTML = "1. Znajdźcie handlarza by kupić detektor chemiczny.";
+          break; 
+      
+          case 2:
+            taskTxt.innerHTML = "2. Poszukajcie pracy by zarobić na detektor chemiczny.";
+          break; 
+      
+          case 3:
+            taskTxt.innerHTML = "3. Odszukajcie Czeczeńskiego najemnika z ogłoszenia o pracę.";
+          break; 
+      
+          case 4:
+            taskTxt.innerHTML = "4. Znajdźcie plany śmigłowca Mi-24.";
+          break; 
+      
+          case 5:
+            taskTxt.innerHTML = "5. Dostarczcie plany do najemnika.";
+          break; 
+      
+          case 6:
+            taskTxt.innerHTML = "6. Kupcie detektor chemiczny.";
+          break; 
+      
+          case 7:
+            taskTxt.innerHTML = "7. Wejdźcie do zony.";
+          break; 
+      
+          case 8:
+            taskTxt.innerHTML = "8. Znajdźcie łapówkę dla rosyjskiego żołnierza.";
+          break; 
+      
+          case 9:
+            taskTxt.innerHTML = "9. Znajdźcie informacje o poszukiwanym artefakcie.";
+              break; 
+              
+        case 10:
+            taskTxt.innerHTML = "10. Zdezaktywujcie ładunki wybuchowe zaznaczone przez stalkera.";
+        break; 
+        case 11:
+        taskTxt.innerHTML = "11. Odszukajcie znajomego stalkera.";
+        break; 
+                    case 12:
+                        taskTxt.innerHTML = "12. Znajdźcie poszukiwany artefakt.";
+                          break; 
+      }
+}
+
+function sw2() {
+    sub()
+    switch (numOne) {
+      case 0:
+        taskTxt.innerHTML = "Kliknij przycisk, aby pokazać treść...";
+        break;
+
+    case 1:
+        taskTxt.innerHTML = "1. Znajdźcie handlarza by kupić detektor chemiczny.";
+      break; 
+  
+      case 2:
+        taskTxt.innerHTML = "2. Poszukajcie pracy by zarobić na detektor chemiczny.";
+      break; 
+  
+      case 3:
+        taskTxt.innerHTML = "3. Odszukajcie Czeczeńskiego najemnika z ogłoszenia o pracę.";
+      break; 
+  
+      case 4:
+        taskTxt.innerHTML = "4. Znajdźcie plany śmigłowca Mi-24.";
+      break; 
+  
+      case 5:
+        taskTxt.innerHTML = "5. Dostarczcie plany do najemnika.";
+      break; 
+  
+      case 6:
+        taskTxt.innerHTML = "6. Kupcie detektor chemiczny.";
+      break; 
+  
+      case 7:
+        taskTxt.innerHTML = "7. Wejdźcie do zony.";
+      break; 
+  
+      case 8:
+        taskTxt.innerHTML = "8. Znajdźcie łapówkę dla rosyjskiego żołnierza.";
+      break; 
+  
+      case 9:
+        taskTxt.innerHTML = "9. Znajdźcie informacje o poszukiwanym artefakcie.";
+          break; 
+          
+    case 10:
+        taskTxt.innerHTML = "10. Zdezaktywujcie ładunki wybuchowe zaznaczone przez stalkera.";
+    break; 
+    case 11:
+    taskTxt.innerHTML = "11. Odszukajcie znajomego stalkera.";
+    break; 
+                case 12:
+                    taskTxt.innerHTML = "12. Znajdźcie poszukiwany artefakt.";
+                      break; 
+      }
+}
+
+
+
